6a4bbf652c12fd53622e91ee276a04eba6d05500
Emil Thorsplass <emilthorsplass@Emils-MacBook-Pro-2.local>
2020-05-30 11:18:24 +0200

    This is my module assignment

---
+++ script.js
@@ -0,0 +1,62 @@
+// question 1
+
+petArray = ["cat", "cow", "dog"];
+
+
+// question 2
+
+console.log(petArray[1]);
+
+
+// question 3
+
+petArray.push("sheep");
+
+
+// question 4
+
+console.log(petArray.length);
+
+
+// question 5
+
+catObject = {
+    name: "Susanna",
+    colour: "orange",
+    age: "8"
+};
+
+
+// question 6
+
+console.log(catObject.age);
+
+
+// question 7
+
+catArray = [
+    {
+        name: "Susanna",
+        colour: "orange",
+        age: "8"
+    }
+];
+
+
+// question 8
+
+for(var i = 0; i < catArray.length; i++) {
+    console.log(catArray[i])
+};
+
+
+// question 9
+
+function logToConsole(someAnimal) {
+    console.log(someAnimal)
+};
+
+
+// question 10
+
+logToConsole("donkey");
